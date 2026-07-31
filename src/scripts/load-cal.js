0afbe8d31361f4a116cd3f8809184b2c3acd1306
Jakin Stahl <jakinstahl@gmail.com>
2026-07-31 14:56:24 -0700

    Tweaked line so it sets hidden field of cal.com

--- src/scripts/load-cal.js
+++ src/scripts/load-cal.js
@@ -40,7 +40,7 @@ function loadCalEmbed() {
     config: { 
       layout: "month_view", 
       useSlotsViewOnSmallScreen: "true",
-      "marketing-consent": String(zaraz?.consent?.get("NtIl") ?? false)
+      marketingconsent: String(zaraz?.consent?.get("NtIl") ?? false)
     },
     calLink: "jakinstahl/project-planning-call",
   });
